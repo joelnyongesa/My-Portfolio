3faaacdd2c3aebfd3d4519fc37338205e74cb3b1
joelnyongesa <joelnyongesa148@gmail.com>
2023-09-02 14:37:21 +0300

    feat: Add the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,25 @@
 const Navbar = () => {
     return ( 
         <div>
-            <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation"></button>
-                <div class="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+                <div className="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About Me</a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownId">
-                                <a class="dropdown-item" href="#">Action 1</a>
-                                <a class="dropdown-item" href="#">Action 2</a>
+                        <li className="nav-item"><a className="nav-link" href="#">Portfolio Projects</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Contact Me</a></li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                            <div className="dropdown-menu" aria-labelledby="dropdownId">
+                                <a className="dropdown-item" href="#">Action 1</a>
+                                <a className="dropdown-item" href="#">Action 2</a>
                             </div>
                         </li>
                     </ul>
-                    <form class="d-flex my-2 my-lg-0">
-                        <input class="form-control me-sm-2" type="text" placeholder="Search"/>
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
                 </div>
             </nav>
         </div>
